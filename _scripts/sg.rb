d5c75809a29cf9f1d6cb17fb652f488c68ae005f
cs <bartus.csongor@gmail.com>
2014-08-30 16:02:36 +0300

    Working hard ...

--- _scripts/sg.rb
+++ _scripts/sg.rb
@@ -1,5 +1,4 @@
 require 'filewatcher'
-require 'fileutils'
 
 root = Dir.pwd
 destination = '_site/assets/styles'
@@ -10,23 +9,26 @@ puts "Watching #{extension} files in #{destination}"
 compile = true
 
 FileWatcher.new(["#{destination}/*#{extension}", "#{destination}/**/*#{extension}"]).watch do |filename|
+  puts "Compile: " + compile.to_s
+  
   if compile 
     # filename: ./_site/assets/styles/atoms/test.liquid
     puts "Updated " + filename
 
     # replace ./_site with absolute path
     absolute = filename.sub "./", "#{root}/"
+    #puts "Absolute " + absolute
 
-    # add .scss to .liquid
-    scss = absolute.sub ".liquid", ".liquid.scss"
-
-    # copy .liquid to .liquid.scss
-    FileUtils.cp_r absolute, scss
-
-    # recompile screen.css with compass
-    puts system('sudo compass compile')
+    # replace .liquid with .scss
+    scss = absolute.sub ".liquid", ".scss"
     
-    # during the compilation the .liquid will be updated again
+    # remove _site
+    scss = scss.sub "_site/", ""
+    #puts "Subs " + scss
+    
+    # copy .liquid to .liquid.scss
+    system("sudo cp -f #{absolute} #{scss}");
+    
     # tell this script do not enter in an endless loop
     compile = false
   else
