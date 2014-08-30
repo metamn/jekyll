792b6b1897eeb2f92459019af1fc5363fec68801
cs <bartus.csongor@gmail.com>
2014-08-30 13:13:57 +0300

    Perfect!

--- _scripts/sg.rb
+++ _scripts/sg.rb
@@ -7,19 +7,29 @@ extension = '.liquid'
 
 puts "Watching #{extension} files in #{destination}"
 
+compile = true
+
 FileWatcher.new(["#{destination}/*#{extension}", "#{destination}/**/*#{extension}"]).watch do |filename|
-  # filename: ./_site/assets/styles/atoms/test.liquid
-  puts "Updated " + filename
-  
-  # replace ./_site with absolute path
-  absolute = filename.sub "./", "#{root}/"
-  
-  # add .scss to .liquid
-  scss = absolute.sub ".liquid", ".liquid.scss"
-  
-  # copy .liquid to .liquid.scss
-  FileUtils.cp_r absolute, scss
-  
-  # recompile screen.css with compass
-  # puts system('sudo compass compile')
+  if compile 
+    # filename: ./_site/assets/styles/atoms/test.liquid
+    puts "Updated " + filename
+
+    # replace ./_site with absolute path
+    absolute = filename.sub "./", "#{root}/"
+
+    # add .scss to .liquid
+    scss = absolute.sub ".liquid", ".liquid.scss"
+
+    # copy .liquid to .liquid.scss
+    FileUtils.cp_r absolute, scss
+
+    # recompile screen.css with compass
+    puts system('sudo compass compile')
+    
+    # during the compilation the .liquid will be updated again
+    # tell this script do not enter in an endless loop
+    compile = false
+  else
+    compile = true
+  end
 end
