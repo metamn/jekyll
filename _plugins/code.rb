1db42edbb0cfaf7985f1252543511b44ae9f0892
cs <bartus.csongor@gmail.com>
2014-08-31 17:28:06 +0300

    The plugin works with SCSS, HTMl and JS too

---
+++ _plugins/code.rb
@@ -0,0 +1,28 @@
+module Jekyll
+  class CodeTag < Liquid::Tag
+
+    def initialize(tag_name, text, tokens)
+      super
+      @text = text.strip
+    end
+
+    def render(context)
+      filename = context.environments.first["page"]["#{@text}"]
+      folder = context.environments.first["page"]["category"]
+      root = Dir.pwd
+      
+      case @text
+        when "scss"
+          file = root + "/assets/styles/" + folder + "/" + filename
+        when "html"
+          file = root + "/_includes/" + folder + "/" + filename
+        when "js"
+          file = root + "/assets/scripts/" + folder + "/" + filename
+      end
+      
+      File.open(file, 'r') { |f| f.read }
+    end
+  end
+end
+
+Liquid::Template.register_tag('code', Jekyll::CodeTag)
